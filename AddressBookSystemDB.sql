2f05435ed29c8349e46474cd3dad736611286386
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-03-05 00:25:45 +0530

    [Saguna] Add Query to Add Type And Address book Name column

--- AddressBookSystemDB.sql
+++ AddressBookSystemDB.sql
@@ -47,3 +47,22 @@ SELECT * FROM AddressBook
 WHERE city = 'Mumbai'
 ORDER BY firstname;
 			
+------- UC-9 Add Type And Name --------
+ALTER TABLE AddressBook
+ADD Book_Name VARCHAR(20),
+    Contact_Type VARCHAR(20);
+
+UPDATE AddressBook
+SET Contact_Type = 'Friends'
+WHERE firstname in ('Sue','Mike');
+
+UPDATE AddressBook
+SET Contact_Type = 'Professional'
+WHERE firstname in ('John','Liz');
+
+UPDATE AddressBook
+SET Contact_Type = 'Family'
+WHERE firstname in ('Meg');
+
+UPDATE AddressBook
+SET City = 'Pune';
