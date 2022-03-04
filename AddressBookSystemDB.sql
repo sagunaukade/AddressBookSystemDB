41466bad6ada15eb14954e47d95ed01aa97555c7
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-03-04 22:52:19 +0530

    [Saguna] Add Query to create Address Book Table

--- AddressBookSystemDB.sql
+++ AddressBookSystemDB.sql
@@ -3,3 +3,15 @@
 --------- UC-1 Create Database ----------
 create database AddressBookSystemDB;
 use AddressBookSystemDB;
+
+-------- UC-2 Create AddressBook Table -----------
+Create Table AddressBook(
+	FirstName varchar(50) not null,
+	LastName varchar(50),
+	Address varchar(255),
+	City varchar(50),
+	StateName varchar(50),
+	ZipCode int,
+	PhoneNum bigint,
+	EmailId varchar(50)
+);
